edfa12a91d65bf588834471faf92cc08b96c2efe
Josef <Youcef9798@gmail.com>
2022-01-19 12:03:29 +0100

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Drawer Demo'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Drawer Demo'),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text(
+                    'Drawer Header',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text('Messages'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title: Text('Profile'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+              ],
             ),
-            drawer: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: const <Widget>[
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                    child: Text(
-                      'Drawer Header',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    ),
+          ),
+          body: Stack(
+            fit: StackFit.loose,
+            children: [
+              new Image.asset('assets/images/GoogleMaps2.png',
+                  height: 1300, width: 1320),
+              Positioned(
+                bottom: 100,
+                right: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('A'),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(24),
                   ),
-                  ListTile(
-                    leading: Icon(Icons.message),
-                    title: Text('Messages'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.account_circle),
-                    title: Text('Profile'),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Settings'),
-                  ),
-                ],
-              ),
-            ),
-            body: Drawer(
-              child: ListView(
-                children: [
-                  Image.asset('assets/images/img_Citadelle.jpg',
-                      height: 1300, width: 1320),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('A'),
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(24),
-                    ),
-                  )
-                ],
-              ),
-            )));
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
