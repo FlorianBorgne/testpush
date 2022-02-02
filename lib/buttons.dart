35390e79f80906cf4af0a5db2990ef0f6ae4023c
Josef <Youcef9798@gmail.com>
2022-02-02 09:12:40 +0100

    Ajout des boutons, InteractiveViwer pour le Zoom

---
+++ lib/buttons.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class ButtonsList extends StatelessWidget {
+  const ButtonsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+      children: [
+        Positioned(
+          bottom: 180,
+          right: 470,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('A'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(5.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 180,
+          right: 518,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('B'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(5.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 225,
+          right: 560,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('C'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(5.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 225,
+          right: 518,
+          width: 15.0,
+          height: 15.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('D'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 263,
+          right: 500,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('E'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 285,
+          right: 470,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('F'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 310,
+          right: 450,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('G'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 335,
+          right: 460,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('H'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 520,
+          right: 565,
+          width: 20.0,
+          height: 20.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('J'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 200,
+          right: 390,
+          width: 15.0,
+          height: 15.0,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('I'),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.red,
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(0.0),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/buttons.dart';
+import 'package:photo_view/photo_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,60 +8,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Drawer Demo'),
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: const <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: Text(
-                    'Drawer Header',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Citadelle'),
+            ),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: const <Widget>[
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      'Nom du Bâtiment',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
                     ),
                   ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.message),
-                  title: Text('Messages'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text('Profile'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-              ],
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title: Text('Messages'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text('Profile'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                  ),
+                ],
+              ),
             ),
-          ),
-          body: Stack(
+            body: Center(
+                child: InteractiveViewer(
+                    boundaryMargin: const EdgeInsets.all(10.0),
+                    // minScale: 0.1,
+                    maxScale: 1.6,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          bottom: 10,
+                          right: 40,
+                          child: Image(
+                            image: AssetImage('assets/images/GoogleMaps.png'),
+                          ),
+                        ),
+                        ButtonsList(),
+                      ],
+                    )))));
+  }
+}
+/**
+ * body: Stack(
             fit: StackFit.loose,
             children: [
-              new Image.asset('assets/images/GoogleMaps2.png',
-                  height: 1300, width: 1320),
+              PhotoView(
+                imageProvider: AssetImage('assets/images/GoogleMaps.png'),
+              ),
               Positioned(
-                bottom: 100,
-                right: 200,
+                bottom: 180,
+                right: 650,
                 child: ElevatedButton(
                   onPressed: () {},
                   child: Text('A'),
                   style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(24),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 180,
+                right: 700,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('B'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
                     shape: CircleBorder(),
                     padding: EdgeInsets.all(24),
                   ),
                 ),
               )
             ],
-          )),
-    );
-  }
-}
+          )
+ */
